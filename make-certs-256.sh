69c1ebd463fc314d01eedbc740c121d6a76ce3ab
Earle F. Philhower, III <earlephilhower@yahoo.com>
2017-03-04 13:08:39 -0800

    Move to SHA256 generated certificates

--- make-certs-256.sh
+++ make-certs-256.sh
@@ -18,8 +18,8 @@ CN = 127.0.0.1
 EOF
 openssl req -out tls.ca_x509.req -key tls.ca_key.pem -new -config certs.conf 
 openssl req -out tls.x509_$BITS.req -key tls.key_$BITS.pem -new -config certs.conf 
-openssl x509 -req -in tls.ca_x509.req  -out tls.ca_x509.pem -sha1 -days 5000 -signkey tls.ca_key.pem 
-openssl x509 -req -in tls.x509_$BITS.req  -out tls.x509_$BITS.pem -sha1 -CAcreateserial -days 5000 -CA tls.ca_x509.pem -CAkey tls.ca_key.pem 
+openssl x509 -req -in tls.ca_x509.req  -out tls.ca_x509.pem -sha256 -days 5000 -signkey tls.ca_key.pem 
+openssl x509 -req -in tls.x509_$BITS.req  -out tls.x509_$BITS.pem -sha256 -CAcreateserial -days 5000 -CA tls.ca_x509.pem -CAkey tls.ca_key.pem 
 openssl x509 -in tls.ca_x509.pem -outform DER -out tls.ca_x509.cer
 openssl x509 -in tls.x509_$BITS.pem -outform DER -out tls.x509_$BITS.cer
 
